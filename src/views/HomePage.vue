4342ce559b02cb3f1057f0c050aaf75bcdcd4a91
chema1998 <105527025+chema1998@users.noreply.github.com>
2024-09-10 20:02:43 -0500

    menu lateral

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,39 @@
 <template>
-  <ion-page>
+  <ion-menu content-id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Opciones</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>This is the menu content.
+      <ion-menu-toggle>
+        <ion-button size="large"expand="full" shape="round">idioma</ion-button>
+        <ion-button size="large"expand="full"shape="round">usuario</ion-button>
+        <ion-button size="large"expand="full"shape="round">tema</ion-button>
+        <ion-button size="large"expand="full"shape="round">cerrar cesion</ion-button>  
+
+      </ion-menu-toggle>
+    </ion-content>
+  </ion-menu>
+
+  <ion-page id="main-content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Menu</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding"> Tap the button in the toolbar to open the menu. </ion-content>
+    <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      </div>
+  </ion-page>
+
+  <!-- <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Blank</ion-title>
@@ -18,11 +52,33 @@
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
-  </ion-page>
+  </ion-page> -->
 </template>
 
-<script setup >
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+  import { IonMenuToggle, IonButtons, IonContent, IonItem, IonHeader, IonIcon, IonList, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  import { heart } from 'ionicons/icons';
+
+  export default defineComponent({
+    components: {
+      IonButtons,
+      IonIcon,
+      IonContent,
+      IonHeader,
+      IonMenu,
+      IonMenuButton,
+      IonPage,
+      IonTitle,
+      IonToolbar,
+      IonList,
+      IonItem,
+      IonMenuToggle,
+    },
+    setup() {
+      return { heart };
+    }
+  });
 </script>
 
 <style scoped>
@@ -53,4 +109,16 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container a {
   text-decoration: none;
 }
+
+ion-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+}
+
+
+
+
+
 </style>
