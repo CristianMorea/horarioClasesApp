8d52d99567cb57695b4cb62b46ad292579acb86d
chema1998 <105527025+chema1998@users.noreply.github.com>
2024-09-16 18:56:36 -0500

    vista ponderado

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 
 import HomePage from '../views/HomePage.vue'
+import ponderado from '../views/ponderado.vue';
 
 const routes=[
   {
@@ -11,6 +12,11 @@ const routes=[
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/ponderado',
+    name: 'ponderado',
+    component: ponderado
   }
 ]
 
