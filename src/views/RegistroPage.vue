1f8e8d040df203942a3a241ee849a239e1d90d84
CristianMorea <120284574+CristianMorea@users.noreply.github.com>
2024-09-19 11:04:14 -0500

    Conexion exitosa con supabase registro funcionando

--- src/views/RegistroPage.vue
+++ src/views/RegistroPage.vue
@@ -1,3 +1,4 @@
+// src/pages/RegistroPage.vue
 <template>
   <ion-page id="registro-page">
     <ion-header>
@@ -8,8 +9,8 @@
     <ion-content>
       <div class="form-container">
         <ion-input placeholder="NOMBRE" v-model="nombre"></ion-input>
-        <ion-input placeholder="CORREO ELECTRÓNICO" v-model="correo"></ion-input>
-        <ion-input placeholder="CONTRASEÑA" v-model="contraseña"></ion-input>
+        <ion-input placeholder="CORREO ELECTRÓNICO" v-model="correo" type="email"></ion-input>
+        <ion-input placeholder="CONTRASEÑA" v-model="contraseña" type="password"></ion-input>
         <ion-button expand="full" @click="handleCrear">Crear</ion-button>
         <ion-button expand="full" @click="handleVolver">Volver</ion-button>
       </div>
@@ -20,6 +21,7 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import { IonInput, IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import supabase from '../supabase';
 
 export default defineComponent({
   components: {
@@ -36,8 +38,21 @@ export default defineComponent({
     const correo = ref('');
     const contraseña = ref('');
 
-    const handleCrear = () => {
-      console.log('Registro creado:', { nombre: nombre.value, correo: correo.value, contraseña: contraseña.value });
+    const handleCrear = async () => {
+      try {
+        const { data, error } = await supabase.auth.signUp({
+          email: correo.value,
+          password: contraseña.value,
+        });
+
+        if (error) throw error;
+
+        console.log('Usuario registrado:', data);
+        alert('Usuario registrado con éxito');
+      } catch (error) {
+        console.error('Error al registrar el usuario:', error.message);
+        alert('Hubo un error al registrar el usuario. Intenta nuevamente.');
+      }
     };
 
     const handleVolver = () => {
