181745a41ed02a3fb66230c44253160d98c9eb6b
chema1998 <105527025+chema1998@users.noreply.github.com>
2024-10-07 19:14:16 -0500

    actualizar inicio sesion

---
+++ src/views/pruebaMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <ion-page>
+      <MenuComponent />
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Vista Principal</ion-title>
+          <ion-buttons slot="start">
+            <ion-menu-button></ion-menu-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content id="main-content">
+        <h1>Bienvenido a la vista principal</h1>
+        <!-- Otro contenido -->
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import MenuComponent from '../components/MenuComponent.vue';
+  
+  export default defineComponent({
+    components: {
+      MenuComponent
+    }
+  });
+  </script>
+  
