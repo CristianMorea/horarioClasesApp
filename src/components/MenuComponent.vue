f45f79ca12721187de537d9be4fddba000680089
chema1998 <105527025+chema1998@users.noreply.github.com>
2024-10-21 17:09:27 -0500

    actualizacion del perfil del usuario

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,45 +1,52 @@
 <template>
-    <ion-menu content-id="main-content" side="start">
-      <ion-content>
-        This is the menu content.
-        <ion-menu-toggle>
-          <ion-button size="large" expand="full" shape="round">idioma</ion-button>
-          <ion-button size="large" expand="full" shape="round">usuario</ion-button>
-          <ion-button size="large" expand="full" shape="round">tema</ion-button>
-          <ion-button size="large" expand="full" shape="round" @click="handleLogout">cerrar sesión</ion-button>
-        </ion-menu-toggle>
-      </ion-content>
-    </ion-menu>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent } from 'vue';
-  import { useRouter } from 'vue-router';
-  import { useStore } from 'vuex';
-  import { IonMenu, IonContent, IonButton, IonMenuToggle } from '@ionic/vue';
-  
-  export default defineComponent({
-    components: {
-      IonMenu,
-      IonContent,
-      IonButton,
-      IonMenuToggle
-    },
-    setup() {
-      const store = useStore();
-      const router = useRouter();
-  
-      const handleLogout = async () => {
-        await store.dispatch('logout'); // Llamar a la acción de logout
-        router.push('/login'); // Redirigir a la página de inicio de sesión
-      };
-  
-      return { handleLogout };
-    }
-  });
-  </script>
-  
-  <style scoped>
-  /* Estilos específicos del componente, si los necesitas */
-  </style>
+  <ion-menu content-id="main-content" side="start">
+    <ion-content>
+      This is the menu content.
+      <ion-menu-toggle>
+        <ion-button size="large" expand="full" shape="round">idioma</ion-button>
+        <ion-button size="large" expand="full" shape="round" @click="goToPerfil">usuario</ion-button>
+        <ion-button size="large" expand="full" shape="round">tema</ion-button>
+        <ion-button size="large" expand="full" shape="round" @click="handleLogout">cerrar sesión</ion-button>
+      </ion-menu-toggle>
+    </ion-content>
+  </ion-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
+import { IonMenu, IonContent, IonButton, IonMenuToggle, menuController } from '@ionic/vue';
+
+export default defineComponent({
+  components: {
+    IonMenu,
+    IonContent,
+    IonButton,
+    IonMenuToggle
+  },
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+
+    const handleLogout = async () => {
+      await store.dispatch('logout'); // Llamar a la acción de logout
+      router.push('/login'); // Redirigir a la página de inicio de sesión
+    };
+
+    const goToPerfil = async () => {
+      await menuController.close(); // Cierra el menú
+      router.push('/modificar-perfil'); // Redirige a la página de modificar perfil
+    };
+
+    return { handleLogout, goToPerfil };
+  }
+});
+</script>
+
+<style scoped>
+/* Estilos específicos del componente, si los necesitas */
+</style>
+
+
   
